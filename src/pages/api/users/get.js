c50813262399487f035a285f9e5db1c35dae8729
dev001 <163705639+rahulmakwana0705@users.noreply.github.com>
2024-04-09 00:55:19 +0530

    updated

--- src/pages/api/users/get.js
+++ src/pages/api/users/get.js
@@ -6,7 +6,6 @@ connectToDatabase();
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            // Retrieve all users from the database
             const users = await User.find();
 
             res.status(200).json({ users });
