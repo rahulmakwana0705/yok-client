f23ff30d7d5caabf4cd7496c6bd15cb717529164
dev001 <163705639+rahulmakwana0705@users.noreply.github.com>
2024-04-08 23:20:46 +0530

    updated

---
+++ src/pages/api/users/get.js
@@ -0,0 +1,20 @@
+import connectToDatabase from '../../../lib/mongodb';
+import User from '../../../models/User';
+
+connectToDatabase();
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            // Retrieve all users from the database
+            const users = await User.find();
+
+            res.status(200).json({ users });
+        } catch (error) {
+            console.error('Error getting users:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        res.status(405).json({ error: 'Method Not Allowed' });
+    }
+}
