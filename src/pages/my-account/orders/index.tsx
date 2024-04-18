b62ea75e3e2cca54ef7f90be98a7c5869e866ae3
dev001 <163705639+rahulmakwana0705@users.noreply.github.com>
2024-04-18 22:49:23 +0530

    Update index.tsx

--- src/pages/my-account/orders/index.tsx
+++ src/pages/my-account/orders/index.tsx
@@ -5,46 +5,63 @@ import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import http from "@framework/utils/http";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
+import Cookies from "js-cookie";
 
 export default function OrdersTablePage({ orders }) {
-	return (
-		<AccountLayout>
-			<OrdersTable orders={orders} />
-		</AccountLayout>
-	);
+  console.log("hello");
+
+  var userData;
+  const authToken = Cookies.get("token");
+  if (authToken) {
+    try {
+      userData = JSON.parse(authToken);
+      console.log("userData", userData);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      console.log("authToken content:", authToken);
+    }
+  }
+  console.log("userData", userData?._id);
+
+  return (
+    <AccountLayout>
+      <OrdersTable orders={orders} />
+    </AccountLayout>
+  );
 }
 
 OrdersTablePage.Layout = Layout;
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-	try {
-		const { data: { orders } } = await http.get(API_ENDPOINTS.ORDERS);
+  try {
+    const {
+      data: { orders },
+    } = await http.get(API_ENDPOINTS.ORDERS);
 
-
-		console.log(orders)
-		return {
-			props: {
-				orders,
-				...(await serverSideTranslations(locale!, [
-					"common",
-					"forms",
-					"menu",
-					"footer",
-				])),
-			},
-		};
-	} catch (error) {
-		console.error("Error fetching orders:", error);
-		return {
-			props: {
-				orders: [],
-				...(await serverSideTranslations(locale!, [
-					"common",
-					"forms",
-					"menu",
-					"footer",
-				])),
-			},
-		};
-	}
+    console.log(orders);
+    return {
+      props: {
+        orders,
+        ...(await serverSideTranslations(locale!, [
+          "common",
+          "forms",
+          "menu",
+          "footer",
+        ])),
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    return {
+      props: {
+        orders: [],
+        ...(await serverSideTranslations(locale!, [
+          "common",
+          "forms",
+          "menu",
+          "footer",
+        ])),
+      },
+    };
+  }
 };
